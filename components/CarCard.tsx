76b04981069cde90458f27fd1704423f9597d8e1
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2023-08-05 16:46:46 +0700

    .

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import { CarProps } from '@/types'
 import { CustomButton, CarDetails } from '.'
-import { calculateCarRent } from '@/utils'
+import { calculateCarRent, generateCarImageUrl } from '@/utils'
 
 interface CarCardProps {
 	car: CarProps
@@ -35,7 +35,7 @@ const CarCard = ({ car }: CarCardProps) => {
 				</p>
 				<div className='relative w-full h-40 my-3 object-contain'>
 					<Image
-						src='/hero.png'
+						src={generateCarImageUrl(car)}
 						alt='car model'
 						fill
 						priority
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types'
+
 export const calculateCarRent = (city_mpg: number, year: number) => {
 	const basePricePerDay = 50 // Base rental price per day in dollars
 	const mileageFactor = 0.1 // Additional rate per mile driven
@@ -29,6 +31,22 @@ export async function fetchCars() {
 	const result = await response.json()
 
 	return result
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+	const url = new URL('https://cdn.imagin.studio/getimage')
+
+	const { make, year, model } = car
+
+	url.searchParams.append('customer', 'hrjavascript-mastery')
+
+	url.searchParams.append('make', make)
+	url.searchParams.append('modelFamily', model.split(' ')[0])
+	url.searchParams.append('zoomType', 'fullscreen')
+	url.searchParams.append('modelYear', `${year}`)
+	url.searchParams.append('angle', `${angle}`)
+
+	return `${url}`
 }
 
 // import { CarProps, FilterProps } from '@types'
